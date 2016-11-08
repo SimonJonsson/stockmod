55cd4473f18852333824de0586c8c49900564432
SimonJonsson <simonjonsson3@hotmail.com>
2016-11-08 23:28:32 +0100

    Made buy/sell changes

--- serverside/webcrawler.py
+++ serverside/webcrawler.py
@@ -18,14 +18,24 @@ for line in file:
     corpList.append(line)
 #passwd = input("Password: ")
 #os.system('cls' if os.name == 'nt' else 'clear')
-print("\rStockmod")
+print("Stockmod")
 file.close()
 
 def fetchData(corp):
     # Reads html and fetches corp name and stock price
     # Will later be used to put into SQL DB
-    response = urllib2.urlopen(corp)
-    html = response.read()
+    conn = False
+    # Here we will need a try-except-catch block for response
+    while (conn == False):
+        try:
+            response = urllib2.urlopen(corp)
+            html = response.read()
+            conn = True
+        except: 
+            conn = False
+            print("No connection to website. Trying to reconnect in 10 seconds.")
+            sleep(10)
+
     html = html.decode('utf-8') #So we can handle it as string
     response.close()
     soup = BS(html,'lxml')
@@ -36,28 +46,58 @@ def fetchData(corp):
     name = name.replace(".","")
     name = name.replace("&","")
     value = soup.find('span', {'class':'pushBox'}).text
+    # buyValue = soup.find('span', {'class':'buyPrice SText bold})
+    # buyValue = buyValue.strip()
+    # buyValue = buyValue.replace("\xa0", "")
+    # buyValue = buyValue.replace(",", "")
+    # sellValue = soup.find('span', {'class':'sellPrice SText bold})
+    # sellValue = sellValue.strip()
+    # sellValue = sellValue.replace("\xa0", "")
+    # sellValue = sellValue.replace(",", "")
+
     value = value.strip()
-    value = value.replace("\xa0", "") # Avanza uses \xa0 as space, therefore the replace
+    value = value.replace("\xa0", "") # Avanza uses \xa0 as space
     value = value.replace(",",".")
     time = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now());
+    # return [name, buyValue, sellValue]
     return [name, time, value]
 
 # Stores stock price, name and timestamp in db
 def storeData(data): # Fix SQL shit l8r
-    db = pymysql.connect(host='95.80.53.172',port=3306,user='Schill', passwd='Hemligt123', db='stockmod')
-    cursor = db.cursor()
+    conn = False
+    while (conn == False):
+        try:
+            db = pymysql.connect(host='95.80.53.172',port=3306,user='Schill', passwd='Hemligt123', db='stockmod')
+            cursor = db.cursor()
+            conn = True
+        except:
+            conn = False
+            print("No connection to database. Trying to reconnect in 10 seconds.")
+            sleep(10)
+            
+        
     for x in data:
-        sql = "INSERT INTO " + x[0] + " (time,value) VALUES (CURRENT_TIMESTAMP()," + x[2] +")"
-        cursor.execute(sql)
-        db.commit()
-    db.close()
-    
+        conn = False
+        while (conn == False):
+            try:
+                sql = "INSERT INTO " + x[0] + " (time,value) VALUES (CURRENT_TIMESTAMP()," + x[2] +")"
+                #sql = "INSERT INTO " + x[0] + " (time,buy,sell) VALUES (CURRENT_TIMESTAMP()," + x[1] +"," + x[2] +  ")"
+                cursor.execute(sql)
+                db.commit() # Needs try-except-catch
+                db.close()
+                conn = True
+            except:
+                conn = False
+                print("No connection to database. Trying to reconnect in 10 seconds.")
+                sleep(10)
+
 # To reinforce that each link is fetched over a period T of time
 def planner():
     length = len(corpList)
     cycle = math.ceil(length/60) + 1 # Round to nearest upper minute
     cycle = cycle*60/length # So we split each download to a cycle, fetches per minute
-    end = datetime.time(17, 30, 00)
+#    end = datetime.time(17, 30, 00)
+    end = datetime.time(20, 30, 00)
     start = datetime.time(9,00,00)
     while True:
         mydate = datetime.datetime.today()
@@ -65,11 +105,12 @@ def planner():
         temp = []
         if (mydate.weekday() != 5 and mydate.weekday() != 6 and start < now and now < end): # If market is open
             print("Open: " + str(now))
+
             for corp in corpList:
                 data = fetchData(corp)
                 temp.append(data)
                 sleep(cycle)
-            storeData(temp)
+                storeData(temp)
             del temp # Release memory, prevent leakage
         else:
             while not (start < now and now < end): # Lowers CPU usage at closed hours
