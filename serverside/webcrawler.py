66094212792ae381e2be16651228150c5efda2f9
SimonJonsson <simonjonsson3@hotmail.com>
2016-11-10 17:49:57 +0100

    lmao

--- serverside/webcrawler.py
+++ serverside/webcrawler.py
@@ -13,7 +13,7 @@ except ImportError:
 file = open('../htmls.txt','r')
 passFile = open('passwd','r')
 passwd = passFile.read()
-print(passwd.replace("\n",""))
+passwd = passwd.replace("\n","")
 corpList = [] # Should be kept global so we don't get alot of calls, saves memory
 # Adds each html in htmls.txt as a corporation
 for line in file:
